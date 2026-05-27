e27f0c76d437ef87760ab247dfba07cd6930a68d
MuuuShin <1248440677@qq.com>
2026-05-27 17:39:15 +0800

    feat: 让输出总成也能正常被染色

--- src/main/java/com/hepdd/gtmthings/common/registry/GTMTRegistration.java
+++ src/main/java/com/hepdd/gtmthings/common/registry/GTMTRegistration.java
@@ -2,19 +2,19 @@ package com.hepdd.gtmthings.common.registry;
 
 import com.gregtechceu.gtceu.api.blockentity.MetaMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.MetaMachine;
-import com.gregtechceu.gtceu.api.machine.multiblock.part.MultiblockPartMachine;
 import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
 
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
 
 import com.hepdd.gtmthings.GTMThings;
+import com.hepdd.gtmthings.common.block.machine.multiblock.part.MEOutputPartMachine;
 
 import java.util.function.Function;
 
 public class GTMTRegistration {
 
-    public static Function<MetaMachineBlockEntity, MetaMachine> ME_OUTPUT = MultiblockPartMachine::new;
+    public static Function<MetaMachineBlockEntity, MetaMachine> ME_OUTPUT = MEOutputPartMachine::new;
 
     public static GTRegistrate GTMTHINGS_REGISTRATE = GTRegistrate.create(GTMThings.MOD_ID);
 
