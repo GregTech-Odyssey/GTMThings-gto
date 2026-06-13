9aefcf3c986e1300a7f02fbab185fb8d47d3d4a9
nutant <65939836+nutant233@users.noreply.github.com>
2026-06-13 14:00:25 +0800

    update

--- src/main/java/com/hepdd/gtmthings/common/block/machine/electric/WirelessEnergyInterface.java
+++ src/main/java/com/hepdd/gtmthings/common/block/machine/electric/WirelessEnergyInterface.java
@@ -53,9 +53,10 @@ public class WirelessEnergyInterface extends TieredIOPartMachine implements IInt
     }
 
     @Override
-    public @Nullable <T> T getGTCapability(Class<T> cap, @Nullable Direction side) {
-        if (cap == GTCapability.ENERGY_CONTAINER && side == null || side == getFrontFacing()) {
-            return cap.cast(energyContainer);
+    public @Nullable <T> Object getGTCapability(Class<T> cap, @Nullable Direction side) {
+        if (cap == GTCapability.ENERGY_CONTAINER) {
+            if (side == null || side == getFrontFacing()) return energyContainer;
+            return GTCapability.EMPTY;
         }
         return super.getGTCapability(cap, side);
     }
